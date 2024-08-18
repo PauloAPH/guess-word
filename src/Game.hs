c034f9b4b3b2683501e583ad79cf474590a1ff2c
Paulo Hayashida <paulo.p.hayashida@gmail.com>
2024-08-18 15:19:23 -0300

    feat: add guess size control

--- src/Game.hs
+++ src/Game.hs
@@ -7,24 +7,23 @@ where
 import Lib
 import System.Random (randomRIO)
 
-data GameState = GameState Int String deriving(Show)
+-- GameState Tentativas Tamanho Palavra
+data GameState = GameState Int Int String deriving(Show)
 
 gameLoop :: GameState -> IO ()
-gameLoop (GameState 0 s) = do
+gameLoop (GameState 0 _max_size s) = do
   print s
   putStrLn "Game Over"
-gameLoop (GameState l s) = do
-  print "Insira a palavra"
-  input2 <- getLine
-  let guess = input2
-  let matches = matchWords guess s 0
+gameLoop (GameState l max_size s) = do
+  guess <- askUserForGuess max_size
+  let matches = matchWords (stringFromGuess guess) s 0
   let result = checkWin matches
   print matches
   case result of
     True -> do
       print s
       putStrLn "Win"
-    False -> gameLoop (GameState (l - 1) s)
+    False -> gameLoop (GameState (l - 1) max_size s)
 
 gameStart :: IO GameState
 gameStart = do
@@ -32,9 +31,9 @@ gameStart = do
   let a = map countLetters (words contents)
   print "Entre com a quantidade de letras"
   input <- getLine
-  let number = read input :: Int
-  let b = listWordsOfSize a number
+  let max_size = read input :: Int
+  let b = listWordsOfSize a max_size
   let size = length b
   randomNumber <- randomRIO (1, size)
   let word = takeWordAt b randomNumber
-  return (GameState 5 word)
+  return (GameState 5 max_size word)
--- src/Lib.hs
+++ src/Lib.hs
@@ -8,10 +8,14 @@ module Lib
     matchWords,
     letterInWord,
     checkWin,
+    stringFromGuess,
+    askUserForGuess,
   )
 where
 
 data WordParam = WordParam String Int deriving (Show)
+
+data Guess = Guess String deriving (Show)
 
 instance Semigroup WordParam where
   (WordParam w1 l1) <> (WordParam w2 l2) = WordParam (w1 ++ w2) (l1 + l2)
@@ -51,3 +55,24 @@ matchWords w w2 n = letterInWord (head w) w2 n ++ matchWords (tail w) w2 (n + 1)
 checkWin :: String -> Bool
 checkWin [] = True
 checkWin s = (head s == 'G') && checkWin (tail s)
+
+guessFromString :: String -> Int -> Maybe Guess
+guessFromString s max_size
+  | length s == max_size = Just (Guess s)
+  | otherwise     = Nothing
+
+-- this is the only way to access the contents of a Name
+stringFromGuess :: Guess -> String
+stringFromGuess (Guess s) = s
+
+askUserForGuess :: Int -> IO Guess
+askUserForGuess
+  max_size = do 
+    print ("Entre com uma palavra de " ++ show max_size ++ " letras")
+    s <- getLine
+    case guessFromString s max_size of
+      Just n  -> return n
+      Nothing -> do
+        print ("Quantidade errada de caracteres tente de novo.")
+        askUserForGuess max_size
+
